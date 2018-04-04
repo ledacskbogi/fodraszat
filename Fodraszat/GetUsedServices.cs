db5bbd500843d53c7b6288696dad8ae7ce1ef78d
szollosinorbertto <szollosi.norbert@t-online.hu>
2018-04-04 21:33:45 +0200

    Unit testek javítása

--- Fodraszat/GetUsedServices.cs
+++ Fodraszat/GetUsedServices.cs
@@ -6,10 +6,16 @@ namespace Fodraszat
     public class GetUsedServices : IGetUsedServices
     {
         private readonly IUsedServiceRepository _usedServiceRepository;
+        private readonly ICustomerManager _customerManager;
 
-        public GetUsedServices(IUsedServiceRepository usedServiceRepository)
+        /// <summary>
+        /// Ez itt a konstruktor
+        /// </summary>
+        public GetUsedServices(IUsedServiceRepository usedServiceRepository, ICustomerManager customerManager)
         {
             _usedServiceRepository = usedServiceRepository;
+            _customerManager = customerManager;
+
         }
 
         /// <summary>  
@@ -20,10 +26,20 @@ namespace Fodraszat
         /// <exception cref="System.InvalidOperationException">Nem létező ügyfélazonosító</exception>
         public IList<ServiceModel> Execute(string customerId)
         {
+          var customers = _customerManager.GetAll();
 
-            // TODO HF: Ezt ki kell fejleszeni
-            // throw new InvalidOperationException("Nem létező ügyfél azonosító.");
-
+            var isMatch = false;
+            foreach (var customer in customers)
+            {
+                if (customer.Id == customerId)
+                {
+                    isMatch = true;
+                }
+            }
+            if (isMatch == false)
+            {
+                throw new InvalidOperationException("Nem létező ügyfél azonosító.");
+            }
 
             var usedServices = _usedServiceRepository.GetByCustomerId(customerId);
             return usedServices;
--- FodraszatTest/GetUsedServicesTest.cs
+++ FodraszatTest/GetUsedServicesTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Fodraszat;
+using NSubstitute;
+using System.Collections.Generic;
 
 namespace FodraszatTest
 {
@@ -11,7 +13,21 @@ namespace FodraszatTest
         public void ExecuteTest_01()
         {
             //Arrange
-            var getUsedServices = new GetUsedServices(new UsedServiceRepository(new FileUtil()));
+
+            // UsedServiceRepository objektum mockolása
+            var usedServiceRepositoryMock = Substitute.For<IUsedServiceRepository>();
+            var services = new List<ServiceModel>();
+            services.Add(new ServiceModel { Id = ServiceKey.Hajvagas, Name = "Hajvágás", TaxRate = 27 });
+            services.Add(new ServiceModel { Id = ServiceKey.BalayageMelir, Name = "Balayage melír", TaxRate = 27 });
+            usedServiceRepositoryMock.GetByCustomerId("01").Returns(services);
+
+            // CustomerManager objektum mockolása
+            var customerManagerMock = Substitute.For<ICustomerManager>();
+            var customers = new List<CustomerModel>();
+            customers.Add(new CustomerModel { Id = "01", Name = "Kiss Macska", BithDate = new DateTime(1990, 05, 01) });
+            customerManagerMock.GetAll().Returns(customers);
+
+            var getUsedServices = new GetUsedServices(usedServiceRepositoryMock, customerManagerMock);
 
             //Act
             var result = getUsedServices.Execute("01");
@@ -29,8 +45,21 @@ namespace FodraszatTest
         [TestMethod]
         public void ExecuteTest_02()
         {
-            //Arrange
-            var getUsedServices = new GetUsedServices(new UsedServiceRepository(new FileUtil()));
+            // Arrange
+            // UsedServiceRepository objektum mockolása
+            var usedServiceRepositoryMock = Substitute.For<IUsedServiceRepository>();
+            var services = new List<ServiceModel>();
+            services.Add(new ServiceModel { Id = ServiceKey.Hajszaritas, Name = "Hajszárítás", TaxRate = 27 });
+            usedServiceRepositoryMock.GetByCustomerId("02").Returns(services);
+
+            // CustomerManager objektum mockolása
+            var customerManagerMock = Substitute.For<ICustomerManager>();
+            var customers = new List<CustomerModel>();
+            customers.Add(new CustomerModel { Id = "02", Name = "Kiss Kutya", BithDate = new DateTime(2010, 05, 01) });
+            customerManagerMock.GetAll().Returns(customers);
+
+            var getUsedServices = new GetUsedServices(usedServiceRepositoryMock, customerManagerMock);
+
 
             //Act
             var result = getUsedServices.Execute("02");
@@ -42,10 +71,21 @@ namespace FodraszatTest
         }
 
         [TestMethod]
-        public void ExecuteTest_03()
+        public void ExecuteTest_04()
         {
-            //Arrange
-            var getUsedServices = new GetUsedServices(new UsedServiceRepository(new FileUtil()));
+            // Arrange
+            // UsedServiceRepository objektum mockolása
+            var usedServiceRepositoryMock = Substitute.For<IUsedServiceRepository>();
+
+            // CustomerManager objektum mockolása
+            var customerManagerMock = Substitute.For<ICustomerManager>();
+            var customers = new List<CustomerModel>();
+            customers.Add(new CustomerModel { Id = "01", Name = "Kiss Macska", BithDate = new DateTime(2010, 05, 01) });
+            customers.Add(new CustomerModel { Id = "02", Name = "Kiss Kutya", BithDate = new DateTime(2010, 05, 01) });
+            customers.Add(new CustomerModel { Id = "03", Name = "Kiss Malac", BithDate = new DateTime(2010, 05, 01) });
+            customerManagerMock.GetAll().Returns(customers);
+
+            var getUsedServices = new GetUsedServices(usedServiceRepositoryMock, customerManagerMock);
 
             //Act, Assert
             // Assert.Throws<Exception>(() => getUsedServices.Execute("03"));
@@ -53,7 +93,7 @@ namespace FodraszatTest
             try
             {
                 //Act
-                getUsedServices.Execute("03");
+                getUsedServices.Execute("04");
 
                 Assert.Fail("Not thrown the expected exception");
             }
