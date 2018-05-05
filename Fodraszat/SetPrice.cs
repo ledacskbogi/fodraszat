034d4e277e75875602fa21b7858e9e64f97325f6
szollosinorbertto <szollosi.norbert@t-online.hu>
2018-05-05 18:02:50 +0200

    HF

---
+++ Fodraszat/SetPrice.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fodraszat
+{
+    public class SetPrice : ISetPrice
+    {
+        private const string dataFilePath = "~\\..\\..\\..\\..\\FodraszatTest\\TestData\\price.txt";
+        private readonly IFileUtil _fileUtil;
+
+        public SetPrice(IFileUtil fileUtil)
+        {
+            _fileUtil = fileUtil;
+        }
+
+        // Ez van a Save helyett, ez hajtja végre a file-ba való írást
+        public void Execute(string serviceId, int price)
+        {
+            //price.txt nevű fájlba kell kiírni json formátumban
+            // a szerializálandó objektum egy List<keyvaluepair<string, int>>, ahol a key a serviceId, a value pedig a price
+            var items = new List<KeyValuePair<string, int>>();
+
+            // Beolvas file-ból (akorábban kiírt árak beolvasása file-ból)
+            var oldJson = _fileUtil.ReadAllText(dataFilePath);
+
+            // Deszerializál
+            if (!string.IsNullOrEmpty(oldJson))
+            {
+                items = JsonConvert.DeserializeObject<List<KeyValuePair<string, int>>>(oldJson);
+            }
+
+            // Amennyiben a régiek között nincsen, hozzáadja az új elemet
+            var foundItems = items.Where(x => x.Key == serviceId).ToList();
+            if (foundItems.Count == 1)
+            {
+                // Van találat tehát felülírjuk a régi árat
+                
+                // TODO Befejezni, Bogikám!
+
+            }
+            else if (foundItems.Count == 0)
+            {
+                // Nincs találat tehát hozzáadjuk az új elemet
+                items.Add(new KeyValuePair<string, int>(serviceId, price));
+            }
+            else
+            {
+                throw new Exception("Sérült az adatfile!");
+            }
+
+            // Szerializál (abban már benne vannak az új ár és a meglévő árak is együttesen)
+            var newJson = JsonConvert.SerializeObject(items);
+
+            // Kiírja file-ba újra (felülírja)
+            _fileUtil.WriteAllText(dataFilePath, newJson);
+        }
+    }
+}
---
+++ FodraszatTest/SetPriceTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fodraszat;
+
+namespace FodraszatTest
+{
+    /// <summary>
+    /// Summary description for SetPriceTest
+    /// </summary>
+    [TestClass]
+    public class SetPriceTest
+    {
+        [TestMethod]
+        public void ExecuteTest()
+        {
+            // Arrange
+            var fileUtil = new FileUtil(); // TODO mockolni!!!
+            var setPrice = new SetPrice(fileUtil);
+
+            // Act
+            setPrice.Execute(ServiceKey.BalayageMelir, 20001);
+            setPrice.Execute(ServiceKey.Hajvagas, 10000);
+
+            // Assert
+            // TODO Mockolás után befejezni
+        }
+
+        [TestMethod]
+        public void Execute_Invalid_Data()
+        {
+            // Arrange
+            var fileUtil = new FileUtil(); // TODO úgy kell mockolni a Fileutilt, hogy a Balayage melírból kettő legyen
+            var setPrice = new SetPrice(fileUtil);
+
+            // Act
+            setPrice.Execute(ServiceKey.BalayageMelir, 20001);
+
+            // Assert
+            // TODO Mockolás után befejezni
+        }
+
+    }
+}
